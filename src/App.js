6eccc34233a73993da71a114664857f1ddfc74c6
Yoonchae92 <j10501@naver.com>
2022-06-13 14:37:32 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import UpNavBar from './Components/UpNavBar';
+import Profile from './Components/Profile';
 
 function App() {
   return (
     <div className="App">
         <UpNavBar/>
       <body>
+        <Profile/>
       </body>
     <footer>
     </footer>
