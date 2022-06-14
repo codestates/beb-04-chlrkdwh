1152c71d136b2c3cd1c391dcb72120c8f685cd9e
ckh7488 <msj74881@gmail.com>
2022-06-14 19:45:04 +0900

    add react-router

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import ConnectWallet from './Components/ConnectWallet';
 
 import styles from './App.module.css'
 import MetaMaskLogin from './Components/MetaMaskLogin';
+import  Home  from './Pages/Home'
+import Explore from './Pages/Explore';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
 
@@ -21,7 +24,14 @@ function App() {
       <div className="App">
         <PrimarySearchAppBar />
         <div className={styles.appBody}>
-        <ConnectWallet metaMask={MetaMaskLogin}></ConnectWallet>
+        {/* <ConnectWallet metaMask={MetaMaskLogin}></ConnectWallet> */}
+        {/* <Explore></Explore> */}
+        <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<Home/>}/>
+              <Route path="explore" element={<Explore/>}/>
+          </Routes>
+        </BrowserRouter>
         
 
         </div>
